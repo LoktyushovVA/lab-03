0acaa3de8bec779cb838fb58224d55caee1e7ade
LoktyushovVA <LoktyushoVA@example.com>
2019-05-26 13:25:01 +0300

    code:Добавлена функция ввода(number ->const)

--- project/main.cpp
+++ project/main.cpp
@@ -2,7 +2,13 @@
 #include <vector>
 
 using namespace std;
-
+//функция функция ввода;number ->const
+vector<double> input_numbers(size_t count) {
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++) {
+        cin >> result[i];
+    }
+    return result;
 int
 main() {
 
