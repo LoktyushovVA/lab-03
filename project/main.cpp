194e55a7359a23da95a5871dcdc14e60fbb794b0
LoktyushovVA <LoktyushoVA@example.com>
2019-05-26 13:47:49 +0300

    code:добавлена функция расчета гистограммы

--- project/main.cpp
+++ project/main.cpp
@@ -9,8 +9,41 @@ vector<double> input_numbers(size_t count) {
         cin >> result[i];
     }
     return result;
+}
+    //функция find_minmax
+
+ void find_minmax(vector<double> numbers, double* min, double* max) {
+    *min = numbers[0];
+    *max = numbers[0];
+    for (double number : numbers) {
+        if (number < *min) {
+            *min = number;
+        }
+        if (number > *max) {
+            *max = number;
+        }
+    }
+}
+//добавлена функция расчета гистограммы
+vector<size_t> make_histogram(const vector<double>& numbers, size_t& bin_count){
+    vector<size_t> bins(bin_count);
+    double min, max;
+    min = max = numbers[0];
+    find_minmax(numbers, &min, &max);
+    for (double number : numbers) {
+        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
+        if (bin == bin_count) {
+            bin--;
+        }
+        bins[bin]++;
+    }
+    return bins;
+}
+
+
 int
-main() {
+main()
+ {
 
     size_t number_count;
     cerr << "Enter number count: ";
